5861f6629ca1fbcc7b2f7bad2f4cdb538a33bd74
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-06-08 08:20:49 +0530

    Clean code of React Vite added

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,16 @@
+import Chai from "./Chai";
+
+function App() {
+  return (
+    <>
+      <Chai />
+      <h1>Chai Aur React - Vite</h1>
+      <h1>
+        Chai Aur React | Chinmay Kaitade - Understanding React Flow and
+        Structure, Making it more Cleaner
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/Chai.jsx
@@ -0,0 +1,5 @@
+function Chai() {
+  return <h1>Chai is Ready - Vite</h1>;
+}
+
+export default Chai;
