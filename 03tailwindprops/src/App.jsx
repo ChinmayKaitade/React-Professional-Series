bde71d369951c87eac5bae5d5e473431c595346a
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-06-12 15:01:34 +0530

    Tailwind Props Project added

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Card from "./components/Card";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  let myObj = {
+    username: "hitesh",
+    age: 21,
+  };
+
+  let newArr = [1, 2, 3];
+
+  return (
+    <>
+      <h1 className="bg-green-400 text-black p-4 rounded-xl mb-4">
+        Tailwind Test
+      </h1>
+
+      <Card username="chaiaurcode" btnText="Click Me" />
+      <Card username="hitesh" />
+    </>
+  );
+}
+
+export default App;
