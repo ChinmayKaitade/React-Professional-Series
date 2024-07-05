9c8b4b3a2bb3d380a441d0cf09689fc2cf2d8bba
ChinmayKaitade <chinmaykaitade123@gmail.com>
2024-07-05 19:30:26 +0530

    Todo Context Project added

---
+++ 10todocontextLocal/src/contexts/TodoContext.js
@@ -0,0 +1,18 @@
+import { createContext, useContext } from "react";
+
+export const TodoContext = createContext({
+  todos: [
+    { id: 1, todo: "Todo Msg", completed: false }
+  ],
+
+  addTodo: (todo) => {},
+  updateTodo: (id, todo) => {},
+  deleteTodo: (id) => {},
+  toggleComplete: (id) => {},
+});
+
+export const useTodo = () => {
+  return useContext(TodoContext);
+};
+
+export const TodoProvider = TodoContext.Provider;
